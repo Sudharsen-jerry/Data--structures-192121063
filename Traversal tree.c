1e023df00a09f16102b0baead660643d42762c82
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2023-08-05 10:55:11 +0530

    Create Traversal tree.c

---
+++ Traversal tree.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+void inorderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+void preorderTraversal(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
+void postorderTraversal(struct Node* root) {
+    if (root != NULL) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    printf("Inorder Traversal: ");
+    inorderTraversal(root);
+    printf("\nPreorder Traversal: ");
+    preorderTraversal(root);
+    printf("\nPostorder Traversal: ");
+    postorderTraversal(root);
+    return 0;
+}
